20d5f3367af0f456252f6f384204938d6d813993
Brendon W <bwherry@midland.k12.ia.us>
2025-10-17 17:02:20 +0000

    gg

---
+++ index.js
@@ -0,0 +1,14 @@
+let firstName = 'Sam';
+
+let jobTitle = 'Printer';
+
+let city = 'LA';
+
+
+
+let newJobPost = `Dear ${firstName},
+Thank you for deciding to join us! 
+Youll be working as our new ${jobTitle} here in ${city}`
+
+
+document.getElementById("result").innerHTML = newJobPost;
